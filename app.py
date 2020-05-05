f7d7fe234006f5faf5c4928186822830d69f731f
EC2 Default User <ec2-user@ip-172-31-80-67.ec2.internal>
2020-05-05 16:24:57 +0000

    added endpoints for sending data back and forth

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 
 app = Flask(__name__)
 room_id_counter=0
+conn_info=dict()
 @app.route('/hello')
 def hello():
     return 'hello there'
@@ -16,11 +17,26 @@ def create():
     return redirect(url)
 
 @app.route('/room/<num>/initiator')
-def initiator():
+def initiator(num):
     return render_template('initiator.html')
 
 @app.route('/room/<num>/observer')
+def observer(num):
     return render_template('observer.html')
 
+@app.route('/room/<num>/signal',methods=['POST'])
+def signal(num):
+    global conn_info 
+    conn_info[num]=['' for _ in range(2)]
+    conn_info[num][0]=request.form['c_info']
+    return 'success!!'
+@app.route('/room/<num>/getsignal',methods=['POST'])
+def getsignal(num):
+    return conn_info[num][0]
+@app.route('/room/<num>/accept',methods=['POST'])
+def accept_conn(num):
+    global conn_info 
+    conn_info[num][1]=request.form['c_info']
+    return 'success!!'
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=80)
