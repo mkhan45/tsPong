2caf2c918051622614d71599a8400ab47ea3c7c2
EC2 Default User <ec2-user@ip-172-31-80-67.ec2.internal>
2020-05-05 15:29:36 +0000

    basic room join buttons working

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 room_id_counter=0
-@app.route('/create_room'):
+@app.route('/hello')
+def hello():
+    return 'hello there'
+@app.route('/')
+def main():
+    return render_template('index.html')
+@app.route('/create_room')
+def create():
+    global room_id_counter
     url='/room/'+str(room_id_counter)+'/initiator'
     room_id_counter+=1
     return redirect(url)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
